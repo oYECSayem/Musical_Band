0cfa3f66c68cadc880ecb03d90ca3142e92beb96
sadia0004 <157726444+sadia0004@users.noreply.github.com>
2024-04-06 00:11:02 +0600

    fan class
    hun

---
+++ Goup07_Simulation_of_a_Musicband/src/sadia_2220645_Fan/Fan.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sadia_2220645_Fan;
+
+/**
+ *
+ * @author USER
+ */
+public class Fan {
+    
+}
