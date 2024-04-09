841a602d04c3efadc247d571e4dd05a058c70ba7
oYECSayem <157726685+oYECSayem@users.noreply.github.com>
2024-04-09 10:17:06 +0600

    ff

---
+++ Goup07_Simulation_of_a_Musicband/src/sayem_2221728_BandMember/CommunicateWithOperationalManagerAndBandPromoterController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package sayem_2221728_BandMember;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+/**
+ * FXML Controller class
+ *
+ * @author USER
+ */
+public class CommunicateWithOperationalManagerAndBandPromoterController implements Initializable {
+
+    @FXML
+    private TextArea sendMessageTA;
+    @FXML
+    private TextArea sendMessageTA1;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    
+}
